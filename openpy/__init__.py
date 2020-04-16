89828a4ca33555dd3762c53944b9b48e4a1a2874
orenovadia <orenovad@gmail.com>
2020-04-16 15:59:16 -0700

    Prettify

--- openpy/__init__.py
+++ openpy/__init__.py
@@ -8,18 +8,6 @@ from urllib.parse import urlparse
 from openpy.filesystem import FileSystem
 
 
-def compression_wrapper(file_name: str, f: BinaryIO):
-    needs_gzip = False
-    if file_name.endswith('.gz') or file_name.endswith('.gzip'):
-        return GzipFile(fileobj=f)
-
-    has_header = binascii.hexlify(f.read(2)) == b'1f8b'
-    f.seek(0)
-    if has_header:
-        return GzipFile(fileobj=f)
-    return f
-
-
 @contextmanager
 def _open_file(file_name):
     """
@@ -29,13 +17,24 @@ def _open_file(file_name):
 
     fs = FileSystem.get_filesystem(parsed.scheme)
 
-    with fs.open(file_name) as f:
-        with compression_wrapper(file_name, f) as f:
-            f = io.TextIOWrapper(f, encoding='utf-8')
+    with fs.open(file_name) as binary_io:
+        with _compression_wrapper(file_name, binary_io) as uncompressed:
+            text = io.TextIOWrapper(uncompressed, encoding='utf-8')
             try:
-                yield f
+                yield text
             finally:
                 pass
+
+
+def _compression_wrapper(file_name: str, f: BinaryIO):
+    if file_name.endswith('.gz') or file_name.endswith('.gzip'):
+        return GzipFile(fileobj=f)
+
+    has_header = binascii.hexlify(f.read(2)) == b'1f8b'
+    f.seek(0)
+    if has_header:
+        return GzipFile(fileobj=f)
+    return f
 
 
 open = _open_file
